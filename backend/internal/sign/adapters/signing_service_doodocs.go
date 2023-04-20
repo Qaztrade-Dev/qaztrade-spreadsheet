98ff1a49066d10bd2ce621208b66ea2b3c4e3a68
Alimukhamed Tlekbai <ali.tlekbai@gmail.com>
2023-04-20 16:48:50 +0600

    fix: sign

--- backend/internal/sign/adapters/signing_service_doodocs.go
+++ backend/internal/sign/adapters/signing_service_doodocs.go
@@ -163,7 +163,7 @@ func (s *signingServiceDoodocsSession) uploadPDF(ctx context.Context, documentNa
 
 func (s *signingServiceDoodocsSession) configureWorkflow(ctx context.Context, documentID string) error {
 	var (
-		url         = fmt.Sprintf("%s/api/v1/documents/%s/workflow/anonymous", s.urlBase, documentID)
+		url         = fmt.Sprintf("%s/api/v1/documents/%s/workflow", s.urlBase, documentID)
 		jsonPayload = []byte(`{
 			"workflow": {
 			  "steps": [
