26948c705d477b09df152da5ee58808c2141e69b
Alimukhamed Tlekbai <ali.tlekbai@gmail.com>
2023-06-29 19:11:34 +0600

    add: adapters

---
+++ backend/internal/assignments/adapters/assignments_repo_pg.go
@@ -0,0 +1,229 @@
+package adapters
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/doodocs/qaztrade/backend/internal/assignments/domain"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/mattermost/squirrel"
+)
+
+type AssignmentsRepositoryPostgres struct {
+	pg *pgxpool.Pool
+}
+
+var _ domain.AssignmentsRepository = (*AssignmentsRepositoryPostgres)(nil)
+
+func NewAssignmentsRepositoryPostgres(pg *pgxpool.Pool) *AssignmentsRepositoryPostgres {
+	return &AssignmentsRepositoryPostgres{
+		pg: pg,
+	}
+}
+
+func (r *AssignmentsRepositoryPostgres) GetInfo(ctx context.Context, input *domain.GetInfoInput) (*domain.AssignmentsInfo, error) {
+	stmt := getAssignmentsInfoQueryStatement(input)
+	sql, args, err := stmt.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	var total, completed uint64
+	err = r.pg.QueryRow(ctx, sql, args...).Scan(&total, &completed)
+	if err != nil {
+		return nil, err
+	}
+
+	output := &domain.AssignmentsInfo{
+		Total:     total,
+		Completed: completed,
+	}
+
+	return output, nil
+}
+
+func getAssignmentsInfoQueryStatement(input *domain.GetInfoInput) squirrel.SelectBuilder {
+	mainStmt := psql.
+		Select(
+			"count(*) as total",
+			"count(*) filter (where ass.is_completed) as completed",
+		).
+		From("assignments ass").
+		Join("users u on u.id = ass.user_id")
+
+	if input.UserID != nil {
+		mainStmt = mainStmt.Where("u.id = ?", *input.UserID)
+	}
+
+	return mainStmt
+}
+
+func (r *AssignmentsRepositoryPostgres) GetMany(ctx context.Context, input *domain.GetManyInput) (*domain.AssignmentsList, error) {
+	total, err := r.getCount(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	objects, err := r.getMany(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	result := &domain.AssignmentsList{
+		Total:   int(total),
+		Objects: objects,
+	}
+
+	return result, nil
+}
+
+func getAssignmentsQueryStatement(input *domain.GetManyInput) squirrel.SelectBuilder {
+	mainStmt := psql.
+		Select(
+			"ass.id",
+			"app.attrs->'application'->>'from'",
+			"app.attrs->'application'->>'bin'",
+			"ass.sheet_title",
+			"ass.sheet_id",
+			"ass.type",
+			"app.link",
+			"u.attrs->>'name'",
+			"ass.rows_from",
+			"ass.rows_until",
+			"ass.rows_total",
+			"assres.total_completed",
+			"ass.is_completed",
+			"ass.completed_at",
+		).
+		From("assignments ass").
+		Join("applications app on app.id = ass.application_id").
+		Join("users u on u.id = ass.user_id").
+		Join("assignment_results assres on assres.id = ass.last_result_id").
+		OrderBy("ass.created_at asc")
+
+	if input.UserID != nil {
+		mainStmt = mainStmt.Where("u.id = ?", *input.UserID)
+	}
+
+	return mainStmt
+}
+
+var psql = squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+
+func (r *AssignmentsRepositoryPostgres) getMany(ctx context.Context, input *domain.GetManyInput) ([]*domain.AssignmentView, error) {
+	stmt := getAssignmentsQueryStatement(input).Limit(input.Limit).Offset(input.Offset)
+	sql, args, err := stmt.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	objects, err := queryAssignmentViews(ctx, r.pg, sql, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range objects {
+		objects[i].Link = fmt.Sprintf("%s#gid=%v", objects[i].Link, objects[i].SheetID)
+		objects[i].RowsFrom += 3
+		objects[i].RowsUntil += 3
+	}
+
+	return objects, nil
+}
+
+func (r *AssignmentsRepositoryPostgres) getCount(ctx context.Context, query *domain.GetManyInput) (uint64, error) {
+	stmt := getAssignmentsQueryStatement(query)
+	sql, args, err := psql.Select("count(*)").FromSelect(stmt, "q").ToSql()
+	if err != nil {
+		return 0, fmt.Errorf("countRows %w", err)
+	}
+
+	var tmp uint64
+	err = r.pg.QueryRow(ctx, sql, args...).Scan(&tmp)
+	if err != nil {
+		err = fmt.Errorf("count query %w", err)
+	}
+
+	return tmp, err
+}
+
+type querier interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	QueryFunc(ctx context.Context, sql string, args []interface{}, scans []interface{}, f func(pgx.QueryFuncRow) error) (pgconn.CommandTag, error)
+}
+
+func queryAssignmentViews(ctx context.Context, q querier, sqlQuery string, args ...interface{}) ([]*domain.AssignmentView, error) {
+	var (
+		objects = make([]*domain.AssignmentView, 0)
+
+		// scans
+		tmpID             *int
+		tmpApplicantName  *string
+		tmpApplicantBIN   *string
+		tmpSheetTitle     *string
+		tmpSheetID        *uint64
+		tmpAssignmentType *string
+		tmpLink           *string
+		tmpAssigneeName   *string
+		tmpRowsFrom       *int
+		tmpRowsUntil      *int
+		tmpRowsTotal      *int
+		tmpRowsCompleted  *int
+		tmpIsCompleted    *bool
+		tmpCompletedAt    *time.Time
+	)
+
+	_, err := q.QueryFunc(ctx, sqlQuery, args, []any{
+		&tmpID,
+		&tmpApplicantName,
+		&tmpApplicantBIN,
+		&tmpSheetTitle,
+		&tmpSheetID,
+		&tmpAssignmentType,
+		&tmpLink,
+		&tmpAssigneeName,
+		&tmpRowsFrom,
+		&tmpRowsUntil,
+		&tmpRowsTotal,
+		&tmpRowsCompleted,
+		&tmpIsCompleted,
+		&tmpCompletedAt,
+	}, func(pgx.QueryFuncRow) error {
+		objects = append(objects, &domain.AssignmentView{
+			ID:             valueFromPointer(tmpID),
+			ApplicantName:  valueFromPointer(tmpApplicantName),
+			ApplicantBIN:   valueFromPointer(tmpApplicantBIN),
+			SheetTitle:     valueFromPointer(tmpSheetTitle),
+			SheetID:        valueFromPointer(tmpSheetID),
+			AssignmentType: valueFromPointer(tmpAssignmentType),
+			Link:           valueFromPointer(tmpLink),
+			AssigneeName:   valueFromPointer(tmpAssigneeName),
+			RowsFrom:       valueFromPointer(tmpRowsFrom),
+			RowsUntil:      valueFromPointer(tmpRowsUntil),
+			RowsTotal:      valueFromPointer(tmpRowsTotal),
+			RowsCompleted:  valueFromPointer(tmpRowsCompleted),
+			IsCompleted:    valueFromPointer(tmpIsCompleted),
+			CompletedAt:    valueFromPointer(tmpCompletedAt),
+		})
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return objects, err
+}
+
+func valueFromPointer[T any](value *T) T {
+	var defaultValue T
+
+	if value == nil {
+		return defaultValue
+	}
+	return *value
+}
---
+++ backend/internal/assignments/adapters/assignments_repo_pg_test.go
@@ -0,0 +1,358 @@
+package adapters
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/doodocs/qaztrade/backend/internal/assignments/domain"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stretchr/testify/suite"
+)
+
+type AssignmentsRepositoryPostgresSuite struct {
+	suite.Suite
+	ctx  context.Context
+	pg   *pgxpool.Pool
+	repo *AssignmentsRepositoryPostgres
+}
+
+func (s *AssignmentsRepositoryPostgresSuite) SetupSuite() {
+	var (
+		err      error
+		ctx      = context.Background()
+		psqlUser = getenv("POSTGRES_USER", "postgres")
+		psqlPass = getenv("POSTGRES_PASS", "postgres")
+		psqlDB   = getenv("POSTGRES_DB", "qaztrade_test")
+		psqlURL  = fmt.Sprintf("postgresql://%s:%s@localhost:5432/%s", psqlUser, psqlPass, psqlDB)
+	)
+
+	s.pg, err = pgxpool.Connect(ctx, psqlURL)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := teardownAssignmentsRepositoryPostgres(ctx, s.pg); err != nil {
+		fmt.Println(err)
+	}
+
+	s.ctx = ctx
+	s.repo = NewAssignmentsRepositoryPostgres(s.pg)
+}
+
+func (s *AssignmentsRepositoryPostgresSuite) TearDownSuite() {
+	s.pg.Close()
+}
+
+func (s *AssignmentsRepositoryPostgresSuite) TearDownTest() {
+	if err := teardownAssignmentsRepositoryPostgres(s.ctx, s.pg); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func TestAssignmentsRepositoryPostgres(t *testing.T) {
+	suite.Run(t, new(AssignmentsRepositoryPostgresSuite))
+}
+
+func applyFixture(ctx context.Context, pg *pgxpool.Pool, fixture func() (string, []string)) error {
+	sql, args := fixture()
+	sqlStr := fmt.Sprintf(sql, toAny(args)...)
+
+	return performSQL(ctx, pg, sqlStr)
+}
+
+func FixtureTestGetMany() (string, []string) {
+	ids := []string{
+		"85925046-290b-4159-9ebb-89c6cdb00fde", // 1
+		"6ae4ead1-1865-44af-a74f-84f612df6f87", // 2
+	}
+
+	sqlQuery := `
+	insert into users 
+		(id, email, hashed_password, attrs)
+	values
+		('%[1]s',	'1',	'',	'{"name": "John Doe"}'),
+		('%[2]s',	'2',	'',	'{"name": "Jack Wolf"}')
+	;
+
+	insert into applications 
+		(id, attrs)
+	values
+		(
+			'%[1]s',
+			'{
+				"application": {
+					"from": "Facebook Inc.",
+					"bin":	"012345678901"
+				}
+			}'::jsonb
+		),
+		(
+			'%[2]s',
+			'{
+				"application": {
+					"from": "Google Ent.",
+					"bin":	"098765432109"
+				}
+			}'::jsonb
+		)
+	;
+
+	insert into "assignments" 
+		(
+			id, created_at,
+			sheet_title, sheet_id, 
+			rows_from, rows_until, 
+			is_completed, completed_at,
+			user_id, application_id
+		)
+	values
+		(
+			1, '2023-01-01T13:30:00',
+			'Sheet1', 1,
+			50, 150,
+			false, null,
+			'%[1]s', '%[1]s'
+		),
+		(
+			2, '2023-01-01T13:31:00',
+			'Sheet2', 2,
+			1, 100,
+			true, '2023-01-02T13:30:00',
+			'%[1]s', '%[2]s'
+		),
+		(
+			3, '2023-01-01T13:32:00',
+			'Sheet1', 1,
+			1, 49,
+			false, null,
+			'%[2]s', '%[1]s'
+		)
+	;
+
+	insert into "assignment_results" 
+		(assignment_id, total_completed)
+	values
+		(1, 0),
+		(2, 100),
+		(3, 14)
+	;
+
+	update "assignments" as ass
+	set
+		last_result_id = assres.id
+	from "assignment_results" as assres
+	where 
+		assres.assignment_id = ass.id
+	;
+	`
+
+	return sqlQuery, ids
+}
+
+func (s *AssignmentsRepositoryPostgresSuite) TestGetMany_All() {
+	s.Require().Nil(applyFixture(s.ctx, s.pg, FixtureTestGetMany))
+
+	var (
+		expAssignmentsTotal  = 3
+		expAssignmentObjects = []*domain.AssignmentView{
+			{
+				ID:             1,
+				ApplicantName:  "Facebook Inc.",
+				ApplicantBIN:   "012345678901",
+				SheetTitle:     "Sheet1",
+				SheetID:        1,
+				AssignmentType: "",
+				Link:           "#gid=1",
+				AssigneeName:   "John Doe",
+				RowsFrom:       53,
+				RowsUntil:      153,
+				RowsTotal:      101,
+				RowsCompleted:  0,
+				IsCompleted:    false,
+				CompletedAt:    time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				ID:             2,
+				ApplicantName:  "Google Ent.",
+				ApplicantBIN:   "098765432109",
+				SheetTitle:     "Sheet2",
+				SheetID:        2,
+				AssignmentType: "",
+				Link:           "#gid=2",
+				AssigneeName:   "John Doe",
+				RowsFrom:       4,
+				RowsUntil:      103,
+				RowsTotal:      100,
+				RowsCompleted:  100,
+				IsCompleted:    true,
+				CompletedAt:    time.Date(2023, time.January, 2, 19, 30, 0, 0, time.Local),
+			},
+			{
+				ID:             3,
+				ApplicantName:  "Facebook Inc.",
+				ApplicantBIN:   "012345678901",
+				SheetTitle:     "Sheet1",
+				SheetID:        1,
+				AssignmentType: "",
+				Link:           "#gid=1",
+				AssigneeName:   "Jack Wolf",
+				RowsFrom:       4,
+				RowsUntil:      52,
+				RowsTotal:      49,
+				RowsCompleted:  14,
+				IsCompleted:    false,
+				CompletedAt:    time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+			},
+		}
+	)
+
+	assignmentsList, err := s.repo.GetMany(s.ctx, &domain.GetManyInput{
+		Limit:  10,
+		Offset: 0,
+	})
+	s.Require().Nil(err)
+	s.Require().Equal(assignmentsList.Total, expAssignmentsTotal)
+	s.Require().Equal(assignmentsList.Objects, expAssignmentObjects)
+}
+
+func (s *AssignmentsRepositoryPostgresSuite) TestGetMany_User() {
+	s.Require().Nil(applyFixture(s.ctx, s.pg, FixtureTestGetMany))
+
+	var (
+		userID               = "85925046-290b-4159-9ebb-89c6cdb00fde"
+		expAssignmentsTotal  = 2
+		expAssignmentObjects = []*domain.AssignmentView{
+			{
+				ID:             1,
+				ApplicantName:  "Facebook Inc.",
+				ApplicantBIN:   "012345678901",
+				SheetTitle:     "Sheet1",
+				SheetID:        1,
+				AssignmentType: "",
+				Link:           "#gid=1",
+				AssigneeName:   "John Doe",
+				RowsFrom:       53,
+				RowsUntil:      153,
+				RowsTotal:      101,
+				RowsCompleted:  0,
+				IsCompleted:    false,
+				CompletedAt:    time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				ID:             2,
+				ApplicantName:  "Google Ent.",
+				ApplicantBIN:   "098765432109",
+				SheetTitle:     "Sheet2",
+				SheetID:        2,
+				AssignmentType: "",
+				Link:           "#gid=2",
+				AssigneeName:   "John Doe",
+				RowsFrom:       4,
+				RowsUntil:      103,
+				RowsTotal:      100,
+				RowsCompleted:  100,
+				IsCompleted:    true,
+				CompletedAt:    time.Date(2023, time.January, 2, 19, 30, 0, 0, time.Local),
+			},
+		}
+	)
+
+	assignmentsList, err := s.repo.GetMany(s.ctx, &domain.GetManyInput{
+		UserID: &userID,
+		Limit:  10,
+		Offset: 0,
+	})
+	s.Require().Nil(err)
+	s.Require().Equal(assignmentsList.Total, expAssignmentsTotal)
+	s.Require().Equal(assignmentsList.Objects, expAssignmentObjects)
+}
+
+func (s *AssignmentsRepositoryPostgresSuite) TestGetInfo_All() {
+	s.Require().Nil(applyFixture(s.ctx, s.pg, FixtureTestGetMany))
+
+	var (
+		expInfo = &domain.AssignmentsInfo{
+			Total:     3,
+			Completed: 1,
+		}
+	)
+
+	assignmentsInfo, err := s.repo.GetInfo(s.ctx, &domain.GetInfoInput{})
+	s.Require().Nil(err)
+	s.Require().Equal(expInfo, assignmentsInfo)
+}
+
+func (s *AssignmentsRepositoryPostgresSuite) TestGetInfo_User() {
+	s.Require().Nil(applyFixture(s.ctx, s.pg, FixtureTestGetMany))
+
+	var (
+		userID  = "85925046-290b-4159-9ebb-89c6cdb00fde"
+		expInfo = &domain.AssignmentsInfo{
+			Total:     2,
+			Completed: 1,
+		}
+	)
+
+	assignmentsInfo, err := s.repo.GetInfo(s.ctx, &domain.GetInfoInput{
+		UserID: &userID,
+	})
+	s.Require().Nil(err)
+	s.Require().Equal(expInfo, assignmentsInfo)
+}
+
+func teardownAssignmentsRepositoryPostgres(ctx context.Context, pg *pgxpool.Pool) error {
+	sqlQuery := `
+		truncate users cascade;
+		truncate applications cascade;
+		truncate assignments cascade;
+		truncate assignment_results cascade;
+	`
+
+	return performSQL(
+		ctx,
+		pg,
+		sqlQuery,
+	)
+}
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
+}
+
+func toAny(ids []string) []any {
+	tmp := make([]interface{}, len(ids))
+	for i, val := range ids {
+		tmp[i] = val
+	}
+	return tmp
+}
+
+//nolint:errcheck
+func performSQL(ctx context.Context, pg *pgxpool.Pool, query string, args ...interface{}) error {
+	tx, err := pg.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx)
+
+	for _, q := range strings.Split(query, ";") {
+		q = strings.TrimSpace(q)
+		if q == "" {
+			continue
+		}
+		if _, err := tx.Exec(ctx, q, args...); err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit(ctx)
+}
